4f3c6d0fb573a3a6e80901a59702fb59606b0656
gerald <oforige@gmail.com>
2022-09-14 13:47:42 +0000

    added a title and pagination to the homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,17 @@
+import { Container, Typography, Stack, Pagination } from '@mui/material'
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>I am the homepage</div>
+    <Container maxWidth="sm">
+        <Typography pt={3} align='center' variant='h5'>Pokemon Challenge</Typography>
+
+
+      <Stack pt={3} direction="row" justifyContent="center">
+        <Pagination count={10} variant="outlined" color="primary" />
+      </Stack>
+    </Container>
+
   )
 }
 
